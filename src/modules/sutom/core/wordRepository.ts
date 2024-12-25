710eae989fd8325cbde56c8f0acba872b4b44b73
Tristan Ardouin <tristan.ardouin@telecomnancy.eu>
2024-12-25 14:59:14 +0100

    ✏️ Fix typo

--- src/modules/sutom/core/wordRepository.ts
+++ src/modules/sutom/core/wordRepository.ts
@@ -9,7 +9,7 @@ class OfflineWordRepository implements WordRepository {
   wordList: string[] = [];
   constructor() {
     const fileContent = fs.readFileSync(
-      'src/modules/sutom/core/mots.filteted.txt',
+      'src/modules/sutom/core/mots.filtered.txt',
       'utf-8',
     );
     this.wordList = fileContent
@@ -27,4 +27,4 @@ class OfflineWordRepository implements WordRepository {
   }
 }
 
-export const wordRepository = new OfflineWordRepository();
+export const wordRepository: WordRepository = new OfflineWordRepository();
