53935ee999dda60f5eb65641a101e6eb20978265
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2025-01-20 15:12:47 +0100

    ci: using dedicated tsup config file, remove production NODE_ENV requirement and include package.json and package-lock.json files in artifact during build workflow

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  minify: true,
+  sourcemap: true,
+  format: 'esm',
+  outExtension: () => ({ js: '.mjs' }),
+  tsconfig: 'tsconfig.json',
+});
