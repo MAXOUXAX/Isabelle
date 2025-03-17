390eaf9d2ae1dfe15a413051d00c3576d66818c6
Ozraam <lhgrillepain@gmail.com>
2025-03-17 18:43:50 +0100

    Making guard clause instead of callback hell

--- src/modules/russian-roulette/commands/russian-roulette.command.ts
+++ src/modules/russian-roulette/commands/russian-roulette.command.ts
@@ -9,7 +9,7 @@ export class RussianRouletteCommand implements IsabelleCommand {
       "Joue à la roulette russe pour avoir une chance d'être banni !",
     );
 
-  executeCommand(interaction: CommandInteraction) {
+  async executeCommand(interaction: CommandInteraction) {
     const guild = interaction.guild;
 
     if (!guild) {
@@ -18,58 +18,36 @@ export class RussianRouletteCommand implements IsabelleCommand {
     }
 
     // choose a random user to be killed between the user that use the command and all the guild member
-    const killed = howIsGonnaBeKilled(interaction.user.id, guild);
+    const killed = getGunTarget(interaction.user.id, guild);
 
-    if (killed) {
-      const member = guild.members.cache.get(killed);
-      member
-        ?.createDM()
-        .then(async (dm) => {
-          await dm.send('Tu as perdu à la roulette russe, RIP');
-
-          // Create an invite to the killed user to join back
-          guild.invites
-            .create(interaction.channelId)
-            .then((invite) => {
-              void dm.send(`https://discord.gg/${invite.code}`);
-              interaction.guild?.members
-                .kick(killed, 'Tu as perdu à la roulette russe, RIP')
-                .catch((e: unknown) => {
-                  console.error(
-                    '[RussianRoulette] Error while kicking user',
-                    e,
-                  );
-                  interaction
-                    .reply('Click ! Tu as survécu à la roulette russe, GG')
-                    .catch((e: unknown) => {
-                      console.error(e);
-                    });
-                });
-              console.log(
-                '[RussianRoulette] Kicked user',
-                killed,
-                mentionId(killed),
-              );
-            })
-            .catch((e: unknown) => {
-              console.error('[RussianRoulette] Error while creating invite', e);
-              void interaction.reply(
-                'Click ! Tu as survécu à la roulette russe, GG',
-              );
-            });
-        })
-        .catch((e: unknown) => {
-          console.error('[RussianRoulette] Error while sending DM', e);
-          interaction.reply('Error while sending DM').catch((e: unknown) => {
-            console.error(e);
-          });
-        });
-    } else {
+    if (killed == null) {
       interaction
         .reply('Click ! Tu as survécu à la roulette russe, GG')
         .catch((e: unknown) => {
           console.error(e);
         });
+      return
+    }
+
+    const member = guild.members.cache.get(killed);
+    try {
+      const dm = await member?.createDM();
+      if (dm) {
+        await dm.send('Tu as perdu à la roulette russe, RIP');
+
+        try {
+          const invite = await guild.invites.create(interaction.channelId);
+          await dm.send(`https://discord.gg/${invite.code}`);
+          await interaction.guild?.members.kick(killed, 'Tu as perdu à la roulette russe, RIP');
+          console.debug('[RussianRoulette] Kicked user', killed, mentionId(killed));
+        } catch (e) {
+          console.error('[RussianRoulette] Error while creating invite or kicking user', e);
+          await interaction.reply('Click ! Tu as survécu à la roulette russe, GG');
+        }
+      }
+    } catch (e) {
+      console.error('[RussianRoulette] Error while sending DM', e);
+      await interaction.reply('Error while sending DM');
     }
   }
 }
@@ -82,7 +60,7 @@ const PERCENTAGES = {
 
 let numberOfGamesSinceLastKill = 0;
 
-function howIsGonnaBeKilled(userID: string, guild: Guild) {
+function getGunTarget(userID: string, guild: Guild) {
   const isSelfKilling = Math.random() > PERCENTAGES.kill_other;
   const killingThreshold = increasePercentageWithLog(
     PERCENTAGES.is_killing,
@@ -108,6 +86,7 @@ function howIsGonnaBeKilled(userID: string, guild: Guild) {
   return null;
 }
 
+// Increase the chance that someone is gonna be killed, plus la suite de commande sans kill augmente plus la chance est elever
 function increasePercentageWithLog(
   maxPercentage: number,
   percentage: number,
@@ -115,10 +94,18 @@ function increasePercentageWithLog(
   return Math.min(
     maxPercentage,
     percentage +
-      mapNumber(Math.log(numberOfGamesSinceLastKill + 1), 0, 4, 0, 1),
+    mapNumber(Math.log(numberOfGamesSinceLastKill + 1), 0, 4, 0, 1),
   );
 }
 
+// map un nombre d'une tranche vers une autre
+// exemple :
+// number : 5
+// in_min : 0
+// in_max : 10
+// out_min : 10
+// out_max : 20
+// return : 15
 function mapNumber(
   number: number,
   in_min: number,
