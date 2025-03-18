044b3d25542c1379cf4aa266deedc201e4119ac0
TanguyFox <tanguyfox@gmail.com>
2025-03-18 17:55:28 +0100

    Init cache management

--- src/utils/schedule.ts
+++ src/utils/schedule.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { config } from '@/config.js';
 import { cacheStore } from '@/utils/cache.js';
 import { VEvent, fromURL } from 'node-ical';
