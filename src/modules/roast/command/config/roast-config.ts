5613796383b35fbba3768dbdbfb660ad1a6adc2a
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2025-10-15 21:57:37 +0200

    fix(roast): isProd variable not properly computed in production

--- src/modules/roast/command/config/roast-config.ts
+++ src/modules/roast/command/config/roast-config.ts
@@ -7,7 +7,7 @@ export const MAX_ROASTS_PER_USER_PER_DAY = Number.isNaN(parsedMaxRoasts)
   ? 2
   : parsedMaxRoasts;
 
-export const isProd = process.env.NODE_ENV === 'production';
+export const isProd = process.env.NODE_ENV !== 'development';
 
 export const ROAST_FALLBACK_MESSAGE =
   'Je suis perdue dans mes notes... Impossible de générer ce roast maintenant. Réessaie plus tard !';
