fd13b9abeba6f00a75dfe4f2b01de570ad6e4647
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2025-10-19 14:30:59 +0200

    chore: remove dead code

--- src/utils/dictionnary.ts
+++
@@ -1,9 +0,0 @@
-//Type générique représentant un dictionnaire de trigger / answer dans les messages
-
-export type MessageDictionnary = Record<
-  string,
-  {
-    trigger: string[];
-    answer: string[];
-  }
->;
