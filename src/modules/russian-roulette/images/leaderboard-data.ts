57ab1d305811770207e96fce5433b724c70c0f19
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2025-11-30 18:57:48 +0100

    chore(eslint): disable some rules inside the leaderboard-data file

--- src/modules/russian-roulette/images/leaderboard-data.ts
+++ src/modules/russian-roulette/images/leaderboard-data.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Collection, GuildMember } from 'discord.js';
 
 const MINUTES_PER_HOUR = 60;
