6e233de9b2ffada4c47c478a1a00961b86240ac5
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2025-12-01 08:54:57 +0100

    fix: wrong package.json path in production (#148)

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,7 +1,13 @@
 import { createRequire } from 'node:module';
+import { environment } from './environment.js';
 
 const require = createRequire(import.meta.url);
-const packageJson = require('../../package.json') as { version: string };
+
+// In development, the package.json file will be in ../../package.json
+// In production, it will be in ./package.json (since the index.mjs and package.json files will be in the root)
+const packageJsonPath =
+  environment === 'development' ? '../../package.json' : './package.json';
+const packageJson = require(packageJsonPath) as { version: string };
 
 /**
  * The current version of the bot, read from package.json.
