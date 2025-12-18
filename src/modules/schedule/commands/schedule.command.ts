9d0daf67e7f6f3e9f37e04037d55e3b3c669fd77
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2025-12-18 13:32:39 +0100

    feat(schedule): get schedule for a specific date

--- src/modules/schedule/commands/schedule.command.ts
+++ src/modules/schedule/commands/schedule.command.ts
@@ -4,6 +4,7 @@ import {
   createLessonEmbed,
   createLessonEmbeds,
   getEndOfTodayLessons,
+  getLessonsFromDate,
   getTodaysLessons,
   getTodaysNextLesson,
   getTomorrowsLessons,
@@ -31,6 +32,17 @@ export class ScheduleCommand implements IsabelleCommand {
     )
     .addSubcommand((subcommand) =>
       subcommand
+        .setName('date')
+        .setDescription("Affiche les cours d'une date spécifique")
+        .addStringOption((option) =>
+          option
+            .setName('date')
+            .setDescription('La date au format JJ/MM/AAAA')
+            .setRequired(true),
+        ),
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
         .setName('cours-suivant')
         .setDescription('Affiche le prochain cours de la journée.'),
     )
@@ -48,6 +60,7 @@ export class ScheduleCommand implements IsabelleCommand {
     const handlers: Record<string, () => Promise<void>> = {
       aujourdhui: () => this.handleTodaysClassesCommand(interaction),
       demain: () => this.handleTomorrowClassesCommand(interaction),
+      date: () => this.handleDateClassesCommand(interaction),
       'cours-suivant': () => this.handleNextClassCommand(interaction),
       'fin-de-journée': () => this.handleEndOfDayCommand(interaction),
     };
@@ -83,6 +96,45 @@ export class ScheduleCommand implements IsabelleCommand {
       await interaction.reply({
         ephemeral: true,
         content: "Demain c'est dodo...\n-# *Aucun cours n'est prévu demain*",
+      });
+      return;
+    }
+
+    await interaction.reply({
+      ephemeral: false,
+      embeds: createLessonEmbeds(lessons),
+    });
+  }
+
+  private async handleDateClassesCommand(
+    interaction: ChatInputCommandInteraction,
+  ): Promise<void> {
+    const dateString = interaction.options.getString('date', true);
+    const dateParts = dateString.split('/').map(Number);
+
+    if (
+      dateParts.length !== 3 ||
+      dateParts[0] < 1 ||
+      dateParts[0] > 31 ||
+      dateParts[1] < 1 ||
+      dateParts[1] > 12 ||
+      dateParts[2] < 1970
+    ) {
+      await interaction.reply({
+        ephemeral: true,
+        content:
+          'Format de date invalide. Veuillez utiliser le format JJ/MM/AAAA.',
+      });
+      return;
+    }
+
+    const date = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+    const lessons = await getLessonsFromDate(date);
+
+    if (lessons.length === 0) {
+      await interaction.reply({
+        ephemeral: true,
+        content: `Aucun cours n'est prévu le ${dateString}.`,
       });
       return;
     }
